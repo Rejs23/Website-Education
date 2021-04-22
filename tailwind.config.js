493c7a61879c5910bced2a25292b2b42d0da79c5
Rejs23 <reskijunaedisalat1@gmail.com>
2021-04-22 16:44:29 +0700

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+       height: {
+       xl: '650px',
+       s: '180px',
+   },
+       maxHeight: {
+       m: "120px",
+   },
+      extend: {
+        width: {
+          's': '320px',
+        },
+      },
+    },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+   corePlugins: {
+      // height: false,
+      // backgroundImage: false,
+     },
+}
